e15c700c2cf744b7279aa68b9817962eff06b466
Medina Maria Jose <mjmedina.c719@gmail.com>
2021-09-08 17:32:23 -0300

    Primeros pasos

---
+++ src/components/auth/NuevaCuenta.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const NuevaCuenta = () => {
+    return (
+        <h2>
+            NuevaCuenta
+        </h2>
+    )
+}
---
+++ src/components/proyectos/Proyectos.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Proyectos = () => {
+    return (
+        <h2>
+            Proyectos
+        </h2>
+    )
+}
